6a6db0b9f018d77fca78e94ee3c56dfe8446e4a5
Guilherme Andre <gui1andre89@gmail.com>
2026-05-10 16:45:20 -0300

    dependency injection de infrastructure e application

---
+++ WizCoDesafio.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WizCoDesafio.Application.Mappings;
+using WizCoDesafio.Application.Pedido.Interfaces;
+using WizCoDesafio.Application.Pedidos;
+
+namespace WizCoDesafio.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(c => c.AddProfile<PedidoMappingProfile>(), typeof(DependencyInjection));
+
+            services.AddScoped<IPedidoService, PedidoService>();
+
+
+            return services;
+        }
+    }
+}
---
+++ WizCoDesafio.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WizCoDesafio.Domain.Interfaces;
+using WizCoDesafio.Infrastructure.Data;
+using WizCoDesafio.Infrastructure.Repositories;
+
+namespace WizCoDesafio.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(op => op.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+
+            return services;
+        }
+    }
+}
