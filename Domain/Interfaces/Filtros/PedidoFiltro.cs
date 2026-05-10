9da82adda73e15820eeff2260135f26f708887f7
Guilherme Andre <gui1andre89@gmail.com>
2026-05-10 16:24:28 -0300

    domain service interfaces

---
+++ Domain/Interfaces/Filtros/PedidoFiltro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WizCoDesafio.Domain.Interfaces.Filtros
+{
+    public record PedidoFiltro(string ClienteNome,
+            DateTime? DataInicio,
+            DateTime? DataFim,
+            decimal ValorMinimo,
+            decimal ValorMaximo,
+            int Pagina = 1,
+            int TamanhoPagina = 20)
+    {
+    }
+}
---
+++ Domain/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WizCoDesafio.Domain.Interfaces.Filtros;
+
+namespace WizCoDesafio.Domain.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        Task CriarAsync(Pedido pedido);
+        Task<IEnumerable<Pedido>> ObterAsync(PedidoFiltro filtro);
+        Task<Pedido?> ObterPorIdAsync(Guid id);
+        Task AtualizarAsync(Pedido pedido);
+        Task RemoverAsync(Pedido pedido);
+
+    }
+}
